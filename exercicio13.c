74327ddfca6fe16b02bccadd7989d474f8a9c3bc
raiannsx <143418422+raiannsx@users.noreply.github.com>
2023-11-06 22:47:30 -0300

    Create exercicio13.c

---
+++ exercicio13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <studlib.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Informe o primeiro número inteiro: ");
+    scanf("%d", &num1);
+
+    printf("Informe o segundo número inteiro: ");
+    scanf("%d", &num2);
+
+    if (num1 == nume2) {
+        printf("Os dois números são iguais: %d\n", numero1);
+    } else if (num1 > num2) {
+        printf("O primeiro número (%d) é o maior e o segundo número (%d) é o menor.\n", numero1, numero2);
+    } else {
+        printf("O segundo número (%d) é o maior e o primeiro número (%d) é o menor.\n", numero2, numero1);
+    }
+
+    return 0;
+}
